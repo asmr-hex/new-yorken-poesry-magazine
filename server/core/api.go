a84549d5029b816f8747df0d5caedb0180e2cfc7
connorwalsh <c@polygon.pizza>
2018-02-06 17:39:33 -0500

    [cw|#4] create api router, stub route handlers

---
+++ server/core/api.go
@@ -0,0 +1,64 @@
+package core
+
+import (
+	"fmt"
+
+	"github.com/gocraft/web"
+)
+
+const (
+	VERSION   = "v1"
+	DASHBOARD = "dashboard"
+)
+
+type API struct {
+	Version string
+	Router  *web.Router
+}
+
+func NewAPI() API {
+	api := API{Version: VERSION}
+
+	api.BuildRouter()
+
+	return api
+}
+
+func (a *API) BuildRouter() {
+	var (
+		prefix     = "/api"
+		pubPrefix  = fmt.Sprintf("%s/%s", prefix, a.Version)
+		dashPrefix = fmt.Sprintf("%s/%s", prefix, DASHBOARD)
+	)
+
+	a.Router = web.New(*a).
+		// TODO include middlewares for authorization etc.
+
+		// Plural type Reads
+		Get(pubPrefix+"/users", (*API).GetUsers).
+		Get(pubPrefix+"/poets", (*API).GetPoets).
+		Get(pubPrefix+"/poems", (*API).GetPoems).
+		Get(pubPrefix+"/issues", (*API).GetIssues).
+		Get(pubPrefix+"/committees", (*API).GetCommittees).
+
+		// User CRUD
+		Post(pubPrefix+"/user", (*API).CreateUser).
+		Get(pubPrefix+"/user/id", (*API).GetUser).
+		Put(pubPrefix+"/user/id", (*API).UpdateUser).
+		Delete(pubPrefix+"/user/id", (*API).DeleteUser).
+
+		// Poet CRD
+		Post(pubPrefix+"/poet", (*API).CreatePoet).
+		Get(pubPrefix+"/poet/id", (*API).GetPoet).
+		Put(pubPrefix+"/poet/id", (*API).UpdatePoet).
+		Delete(pubPrefix+"/poet/id", (*API).DeletePoet).
+
+		// Poem R (Poems can only be read via the API)
+		Get(pubPrefix+"/poem/id", (*API).GetPoem).
+
+		// Issue R (Issues can only be read via the API)
+		Get(pubPrefix+"/issue/id", (*API).GetIssue).
+
+		// Committee R (Committees can only be read via the API)
+		Get(pubPrefix+"/committee/id", (*API).GetCommittee)
+}
--- server/server.go
+++ server/server.go
@@ -6,34 +6,25 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/connorwalsh/new-yorken-poesry-magazine/server/core"
 	"github.com/fatih/color"
-	"github.com/gocraft/web"
 )
 
 var (
 	arg = flag.String("arg", "123", "ok")
 )
 
-type Context struct {
-}
-
-func (c *Context) PrintShit(rw web.ResponseWriter, req *web.Request) {
-	fmt.Println("OHOHOHHO")
-}
-
 func main() {
-
 	flag.Parse()
 
 	// check args
 
-	// define API routes
-	router := web.New(Context{}).
-		Get("/", (*Context).PrintShit)
+	// init API routers + state
+	api := core.NewAPI()
 
 	fmt.Printf(color.GreenString("New Yorken Poesry daemon running...\n"))
 
-	err := http.ListenAndServe("localhost:3000", router)
+	err := http.ListenAndServe("localhost:3000", api.Router)
 	if err != nil {
 		log.Fatal(err)
 	}
