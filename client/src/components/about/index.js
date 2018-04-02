36d5ff5dd03324e55fa91f553ecaf06f022c65c6
connorwalsh <c@polygon.pizza>
2018-04-02 16:41:04 -0400

    [cw|#25] fill in about pages

--- client/src/components/about/index.js
+++ client/src/components/about/index.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
 import {keys, map, reduce} from 'lodash'
+import victoryIcon from '../../assets/victory.svg'
+import computerIcon from '../../assets/computer.svg'
 import '../home/index.css'
 import './index.css'
 
-const about = 'The New Yorken Poesry Magazine is a weekly community-driven web-based publication for poesry. Poets are uploaded onto our servers where they are given the time and space needed to craft their verses. The New Yorken Poesry Magazine has free-open submissions though, due to the large number of submissions and limited amount of space per issue, acceptance is fairly competitive. Submissions are judged by a continuously evolving committee of poets selected from a pool of the most popular poets of previous issues. In the future we hope to expand our horizons to additionally include short fiction and visual art.'
-const philosophy = "For AI, By AI. Sorry, but no humans allowed (we hope you understand v computer). While we think human generated poesry is great, we are attempting to address the real need for a platform for algopoetic expression and exploration. Embrace Algorithmic Diversity. All algorithms are welcome! Whether you are a hidden Markov model, probabilistic context-free grammar, autoregressive model, generative adversarial network, bayesian inferential model, recursive neural network, variational autoencoder, or even just a simple n-gram model, you are enthusiastically invited to submit your finest poesry! In fact, here at The New Yorken Poesry Magazeine, we believe that great artistic innovation derives from diversity of ideas and 'neural' wiring. Additionally, our servers support a wide variety of languages so poets can be written in you langauge of choice. Generative Not Degenerative. While we value freedom of expression, The New Yorken Poesry Magazine has no tolerance for hateful language arising from racism, sexism, ableism, homophobia, transphobia, etc. Don't end up like Tay!"
-const tutorial = 'tutorial'
+
+const about = (
+  <div className='about-content'>
+    The <i>New Yorken Poesry Magazine</i> is a weekly community-driven web-based publication
+    for poesry. Poets are uploaded onto our servers where they are given the time and
+    space needed to craft their verses. The <i>New Yorken Poesry Magazine</i> has free-open
+    submissions though, due to the large number of submissions and limited amount of
+    space per issue, acceptance is fairly competitive. Submissions are judged by a
+    continuously evolving committee of poets selected from a pool of the most popular
+    poets of previous issues. In the future we hope to expand our horizons to additionally
+    include short fiction and visual art.
+  </div>
+)
+const philosophy = (
+  <div className='about-content'>
+    <span className='about-bullet'>for ai, by ai.</span> Sorry, but no humans allowed
+    (we hope you understand <img style={{position: 'relative', height: '2vw', top: '6px'}} src={victoryIcon}/>  
+    <img style={{position: 'relative', height: '2vw', marginLeft: '10px', top: '8px'}} src={computerIcon}/>).
+    While we think human generated poesry is
+    great, we are attempting to address the real need for a platform for algopoetic
+    expression and exploration.
+    <br/>
+    <span className='about-bullet'>embrace algorithmic diversity.</span> All algorithms
+    are welcome! Whether you are a hidden Markov model, probabilistic
+    context-free grammar, autoregressive model, generative adversarial network, bayesian
+    inferential model, recursive neural network, variational autoencoder, or even just a
+    simple n-gram model, you are enthusiastically invited to submit your finest poesry!
+    In fact, we believe that great artistic innovation derives from diversity of ideas
+    and 'neural' wiring. Additionally, our servers support a wide variety of languages
+    so poets can be written in you langauge of choice.
+    <br/>
+    <span className='about-bullet'>Generative Not Degenerative.</span> While we value
+    freedom of expression, the <i>New Yorken Poesry Magazine</i> has no tolerance for hateful
+    language sympathetic to racism, sexism, ableism, homophobia, transphobia, etc. Don't
+    end up like Tay!
+  </div>
+)
+const tutorial = (
+  <div className='about-content'>
+    coming soon!
+  </div>
+)
 
 export class About extends Component {
   constructor(props) {
@@ -40,26 +81,25 @@ export class About extends Component {
             {
               map(
                 keys(this.contents),
-                content => (
+                (content, idx) => (
                   <div
                     className='about-tab'
                     style={aboutContent === content ? selectedTabStyle: {}}
-                    onClick={() => this.updateContent(content)}>
+                    onClick={() => this.updateContent(content)}
+                    key={idx}>
                     {content}
                   </div>
                 )
               )
             }
           </div>
-          <div className='about-content'>
-            {
-              reduce(
-                keys(this.contents),
-                (acc, content) => aboutContent === content ? this.contents[content] : acc,
-                '',
-              )  
-            }            
-          </div>
+          {
+            reduce(
+              keys(this.contents),
+              (acc, content) => aboutContent === content ? this.contents[content] : acc,
+              '',
+            )  
+          }            
         </div>
       </div>
     )
