c8f956ab1ce6c92f22a1570f1a5ce4e7478e7cbb
connorwalsh <c@polygon.pizza>
2018-04-23 21:27:38 -0400

    working on redux forms

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
+import { Field, reduxForm } from 'redux-form'
 
 
 class login extends Component {
+  submit = values => {
+    console.log(values)
+  }
+
   render() {
     return (
       <div>
-        singing in
+        <LoginForm onSubmit={this.submit}/>
       </div>
     )
   }
 }
+
+const loginForm = props => {
+  const { handleSubmit } = props
+  return (
+    <form onSubmit={handleSubmit}>
+        <div>
+            <label htmlFor="firstName">First Name</label>
+            <Field name="firstName" component="input" type="text" />
+        </div>
+        <button type="submit">Submit</button>
+    </form> 
+  )
+}
+
+export const LoginForm = reduxForm({
+  form: 'login'
+})(loginForm)
 
 export const Login = connect(() =>({}), {})(login)
--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,5 +1,6 @@
 import {combineReducers} from 'redux'
 import {ui} from './ui'
+import {login} from './login'
 
 
 export const reducers = combineReducers({
