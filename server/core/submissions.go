68ddbcd43ee37b54b44b4fba9b306a2cfa4bf839
connorwalsh <c@polygon.pizza>
2018-09-13 09:45:36 -0400

    [cw] partially implement `SelectWinningPoems`
    Major Changes:
    * fill in `SelectWinningPoems` (INCOMPLETE)
    * add supporting fields for coordinating concurrent critics
    * introduce `updateBestPoems` (INCOMPLETE)

--- server/core/submissions.go
+++ server/core/submissions.go
@@ -19,12 +19,13 @@ const (
 
 type SubmissionService struct {
 	*Logger
-	ExecContext        *env.ExecContext
-	Period             time.Duration
-	numConcurrentExecs int
-	wait               chan bool
-	limboDir           string
-	db                 *sql.DB
+	ExecContext             *env.ExecContext
+	NumPoemsInUpcomingIssue int
+	Period                  time.Duration
+	numConcurrentExecs      int
+	wait                    chan bool
+	limboDir                string
+	db                      *sql.DB
 }
 
 func NewSubmissions(execCtx *env.ExecContext, db *sql.DB) *SubmissionService {
@@ -149,16 +150,105 @@ func (s *SubmissionService) ElicitPoemsFrom(poets []*types.Poet) {
 }
 
 func (s *SubmissionService) SelectWinningPoems() {
+	var (
+		bestPoems = []*types.Poem{}
+		err       error
+	)
+
 	issue, err := types.GetUpcomingIssue(s.db)
 	if err != nil {
 		s.Error(err.Error())
 	}
 
-	_ = issue
-
 	// for each poem-committe-member pair, generate score
 
+	// read in all limbo files
+	files, err := filepath.Glob(filepath.Join(s.limboDir, "*.txt"))
+	if err != nil {
+		s.Error(err.Error())
+	}
+
 	// iterate through filenames in limbo dir
+	for _, file := range files {
+		bytes, err := ioutil.ReadFile(file)
+		if err != nil {
+			s.Error(err.Error())
+		}
+
+		// initialize the candidate poem and its scores
+		poem := types.Poem{}
+		scores := []float64{}
+
+		// since we are running the critics in a go routine pool,
+		// we need to create a channel for them to submit their
+		// scores to and a channel to tell the score collector
+		// when to stop.
+		submit := make(chan float64)
+		done := make(chan bool)
+
+		err = json.Unmarshal(bytes, &poem)
+		if err != nil {
+			s.Error(err.Error())
+		}
+
+		// define a function that will take a poet judge
+		// and will critique the poem in question
+		f := func(critic *types.Poet) {
+			// when the critic is done, get out of line
+			defer func() { <-s.wait }()
+
+			// set execution config
+			critic.ExecContext = s.ExecContext
+
+			score, err := critic.CritiquePoem(poem.Content)
+			if err != nil {
+				s.Error(err.Error())
+			}
+
+			// submit this score
+			submit <- score
+		}
+
+		// start score collector
+		go func() {
+			for {
+				select {
+				case score := <-submit:
+					// a score has been submitted for this poem
+					// add it to the array of scores! Wahooo
+					scores = append(scores, score)
+				case <-done:
+					// all the critics are finished scoring!
+					return
+				}
+			}
+		}()
+
+		// ( ͡° ͜ʖ ( ͡° ͜ʖ ( ͡° ͜ʖ ( ͡° ͜ʖ ͡°) ͜ʖ ͡°)ʖ ͡°)ʖ ͡°)
+		// send each critic into the critical void (e.g. go routine pool)
+		// to critique these poetic verses! ( ･_･)♡
+		for _, critic := range issue.Committee {
+			// wait in line for judgment
+			s.wait <- true
+
+			// execute poet
+			go f(critic)
+		}
+
+		// the critics have finished scoring!
+		done <- true
+
+		// okay, whew (｡-_-｡), now we must average the scores for this poem
+		var total float64 = 0
+		for _, score := range scores {
+			total += score
+		}
+		poem.Score = total / float64(len(scores))
+
+		// and decide whether it should be inserted into the best poems!
+		bestPoems = s.updateBestPoems(bestPoems, &poem)
+	}
+
 	//   - unmarshal each into a Poem struct
 	//   - have each committee member judge this poem
 	//   - average each committee members scores
@@ -171,6 +261,44 @@ func (s *SubmissionService) SelectWinningPoems() {
 	// now we have a map of the n highest rated poems
 
 	// store these poems in the database
+}
+
+func (s *SubmissionService) updateBestPoems(bestPoems []*types.Poem, poem *types.Poem) []*types.Poem {
+	var (
+		newBestPoems = []*types.Poem{}
+	)
+
+	// if there are enough slots, just add this poem!
+	if len(bestPoems) < s.NumPoemsInUpcomingIssue {
+		// insert this poem s.t. array is sorted
+		for idx, bestPoem := range bestPoems {
+			if poem.Score >= bestPoem.Score {
+				// this is really easy to parse, right?
+				newBestPoems = append(
+					newBestPoems,
+					append(
+						[]*types.Poem{poem},
+						newBestPoems[idx:]...,
+					)...,
+				)
+
+				return newBestPoems
+			}
+
+			// otherwise, just keep appending to the newBestPoems
+			newBestPoems = append(newBestPoems, bestPoem)
+		}
+	}
+
+	for _, bestPoem := range bestPoems {
+		switch {
+		case poem.Score > bestPoem.Score:
+			// okay we *must* include this poem in the next issue,
+			// it is simply fabulous!
+		}
+	}
+
+	return newBestPoems
 }
 
 func (s *SubmissionService) ReleaseNewIssue() {
