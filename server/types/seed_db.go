867f2b2251c8f5eb24dd75bb69e98246fd7fc765
connorwalsh <c@polygon.pizza>
2018-09-15 20:25:36 -0400

    [cw] introduce SEED_DB config option, scaffold SeedDB function

---
+++ server/types/seed_db.go
@@ -0,0 +1,19 @@
+package types
+
+import "database/sql"
+
+// SeedDB is used to initially seed the database with test data.
+func SeedDB(db *sql.DB) error {
+
+	// populate user tables
+
+	// populate poet tables
+
+	// populate poem tables
+
+	// populate issue tables
+
+	// populate link tables (you know the ones)
+
+	return nil
+}
