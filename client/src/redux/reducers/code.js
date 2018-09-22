70307f170946fc4336810abdacf97a22d4e9ad34
connorwalsh <c@polygon.pizza>
2018-09-22 19:56:21 -0400

    [cw] code highlighting

--- client/src/redux/reducers/code.js
+++ client/src/redux/reducers/code.js
@@ -4,7 +4,9 @@ import {READ_POET_CODE_SUCCESSFUL} from '../actions/poets'
 export const codeByPoetId = (state={}, action) => {
   switch (action.type) {
   case READ_POET_CODE_SUCCESSFUL:
-    return {...state, [action.payload.poetId]: action.payload.code}
+    // convert code bytes to utf8 characters
+    const code = action.payload.code
+    return {...state, [action.payload.poetId]: code}
   default:
     return state
   }
